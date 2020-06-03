f4e8c67e97ee5a14ea275247e504e4e5c46076cb
Gizmeon Dev <firoze@gizmeon.com>
2020-06-03 19:27:33 +0530

    push

--- src/Containers/AddConatct/addContact.js
+++ src/Containers/AddConatct/addContact.js
@@ -12,9 +12,17 @@ import {Header, ListItem, Button, Input} from 'react-native-elements';
 import {Colors} from '../../Theme';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import ImagePicker from 'react-native-image-picker';
+import DocumentPicker from 'react-native-document-picker';
+import {log} from 'react-native-reanimated';
 
 function addContact(props) {
-  const [imageFilePath, setimageFilePath] = useState('plus');
+  const [imageFilePath, setimageFilePath] = useState({
+    image: 'plus',
+    imageName: '',
+  });
+  const [documentFilePath, setdocumentFilePath] = useState(false);
+  const [documentFileName, setdocumentFileName] = useState('');
+
   const chooseFile = () => {
     var options = {
       title: 'Select Image',
@@ -24,6 +32,7 @@ function addContact(props) {
         path: 'images',
       },
     };
+
     ImagePicker.launchImageLibrary(options, response => {
       console.log('Response = ', response);
 
@@ -36,15 +45,80 @@ function addContact(props) {
         alert(response.customButton);
       } else {
         let source = response;
-        console.log('ImagePicker data: ', response.data);
+        console.log('ImagePicker data: ', response);
         console.log('ImagePicker uri: ', response.uri);
+        console.log('ImagePicker uri66666666: ', response.fileName);
         // You can also display the image using data:
         // let source = { uri: 'data:image/jpeg;base64,' + response.data };
-        setimageFilePath(source);
-        console.log('ImagePicker : ', imageFilePath);
+        setimageFilePath({image: source, imageName: response.fileName});
+
+        console.log('ImagePicker : ', imageFilePath.image);
       }
     });
   };
+
+  const selectOneFile = async selectionType => {
+    try {
+      const res = await DocumentPicker.pick({
+        type:
+          selectionType === 'image'
+            ? [DocumentPicker.types.images]
+            : [DocumentPicker.types.pdf],
+      });
+      console.log(
+        res.uri,
+        res.type, // mime type
+        res.name,
+        res.size,
+      );
+
+      var temp = new Array();
+      var temp1 = new Array();
+      // this will return an array with strings "1", "2", etc.
+      //
+
+      temp = res.name.split('_');
+      if (temp.length === 3) {
+        temp1 = temp[2].split('.');
+      }
+      if (
+        temp.length === 3 &&
+        temp[0] === 'Dbc' &&
+        temp[1].length === 6 &&
+        temp1.length === 2
+      ) {
+        if (selectionType === 'image') {
+          if (temp1[0] === 'image') {
+            setimageFilePath({image: res.uri, imageName: res.name});
+          } else {
+            alert('this is not correct format');
+          }
+          console.log('@@@@@@@@@@@', temp.length);
+          console.log('@@@@@@@@@@@', temp[1].length);
+          console.log('@@@@@@@@@@@', temp1);
+        } else {
+          if (temp1[0] === 'pdf') {
+            setdocumentFilePath(true);
+            setdocumentFileName(res.name);
+          } else {
+            alert('this is not correct format');
+          }
+        }
+      } else {
+        alert('this is not correct format');
+      }
+    } catch (err) {
+      //Handling any exception (If any)
+      if (DocumentPicker.isCancel(err)) {
+        //If user canceled the document selection
+      } else {
+        //For Unknown Error
+
+        throw err;
+      }
+    }
+  };
+
   return (
     <View>
       <Header
@@ -69,9 +143,9 @@ function addContact(props) {
         />
         <View>
           <Text style={styles.Text}>Add Conatct</Text>
-          {imageFilePath == 'plus' ? (
+          {imageFilePath.image == 'plus' ? (
             <Button
-              onPress={chooseFile}
+              onPress={() => selectOneFile('image')}
               buttonStyle={{height: 100, width: 100, marginLeft: 8.0}}
               title=""
               titleStyle={{marginRight: 5}}
@@ -80,24 +154,51 @@ function addContact(props) {
               iconRight
             />
           ) : (
-            <Image
-            source={{ uri: imageFilePath.uri }}
-            style={{ width: 100, height: 100, marginLeft: 8.0}}
-          />
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Image
+                source={{uri: imageFilePath.image}}
+                style={{width: 100, height: 100, marginLeft: 8.0}}
+              />
+              <Text
+                style={{
+                  flexDirection: 'row',
+                  alignSelf: 'center',
+                  marginLeft: 10.0,
+                }}>
+                {imageFilePath.imageName}
+              </Text>
+            </View>
           )}
         </View>
         <View>
           <Text style={styles.Text}>Add Document</Text>
+    <View style={{flexDirection: 'row', alignItems: 'center'}}>
           <Button
-              onPress={chooseFile}
-              buttonStyle={{height: 100, width: 100, marginLeft: 8.0}}
-              title=""
-              titleStyle={{marginRight: 5}}
-              icon={<Icon name="plus" size={50} color="white" />}
-              iconContainerStyle={{backgroundColor: 'red', paddingLeft: 10}}
-              iconRight
-            />
-        
+            onPress={() => selectOneFile('pdf')}
+            buttonStyle={{height: 100, width: 100, marginLeft: 8.0}}
+            title=""
+            titleStyle={{marginRight: 5}}
+            icon={
+              <Icon
+                name={documentFilePath ? 'file' : 'plus'}
+                size={50}
+                color="white"
+              />
+            }
+            iconContainerStyle={{backgroundColor: 'red', paddingLeft: 10}}
+            iconRight
+          />
+          {documentFilePath && (
+            <Text
+              style={{
+                flexDirection: 'row',
+                alignSelf: 'center',
+                marginLeft: 10.0,
+              }}>
+              {documentFileName}
+            </Text>
+          )}
+          </View>
         </View>
       </View>
     </View>
@@ -126,7 +227,7 @@ const styles = StyleSheet.create({
   inputDescriptionContainer: {
     borderRadius: 10,
     borderColor: Colors.grey,
-    height: 90,
+    height: 45,
     borderWidth: 0.5,
     paddingLeft: 7.0,
     textAlignVertical: 'top',
